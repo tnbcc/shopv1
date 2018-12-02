049e3562e7b951c73f8974b0fa1921824288261d
tnbcc <tniub.cc@gmail.com>
2018-12-02 13:31:57 +0000

    触发事件监听注册邮件激活

---
+++ app/Listeners/RegisteredListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Notifications\EmailVerificationNotification;
+use Illuminate\Auth\Events\Registered;
+
+class RegisteredListener implements ShouldQueue
+{
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        $user = $event->user;
+
+        $user->notify(new EmailVerificationNotification());
+    }
+}
