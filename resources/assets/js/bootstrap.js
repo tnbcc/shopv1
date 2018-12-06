6a38fdac9b9adad9d08123185bff5940755b3bfe
tnbcc <tniub.cc@gmail.com>
2018-12-06 14:28:36 +0000

    修改和删除收货地址&policy的使用

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -37,6 +37,8 @@ if (token) {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
+require('sweetalert');
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
