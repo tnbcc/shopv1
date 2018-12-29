5e1eb92a6e08f15ef4900c73a8a3c201f21dc75d
tnbcc <tniub.cc@gmail.com>
2018-12-29 08:57:15 +0000

    商品详情页

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,4 @@ Route::group(['middleware' => 'auth'], function() {
 });
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}','ProductsController@show')->name('products.show');
