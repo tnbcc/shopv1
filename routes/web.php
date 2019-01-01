95989a5b13e38e28a8661dfe7b0c890968a3ffc8
tnbcc <tniub.cc@gmail.com>
2019-01-01 05:50:37 +0000

    商品添加购物车

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::group(['middleware' => 'auth'], function() {
         Route::delete('user_addresses/{address}','UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        Route::post('cart', 'CartController@add')->name('cart.add');
     });
     // 结束
 });
