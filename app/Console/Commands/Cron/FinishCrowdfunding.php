898527655dd91c2aa001122fb31c0d3c274e09d7
tnbcc <tniub.cc@gmail.com>
2019-02-14 14:32:14 +0000

    修改

--- app/Console/Commands/Cron/FinishCrowdfunding.php
+++ app/Console/Commands/Cron/FinishCrowdfunding.php
@@ -34,18 +34,22 @@ class FinishCrowdfunding extends Command
     public function handle()
     {
         CrowdfundingProduct::query()
+            // 预加载商品数据
             ->with(['product'])
+            // 众筹结束时间早于当前时间
             ->where('end_at', '<=', Carbon::now())
+            // 众筹状态为众筹中
             ->where('status', CrowdfundingProduct::STATUS_FUNDING)
             ->get()
             ->each(function (CrowdfundingProduct $crowdfunding) {
-                //目标金额大于实际金额
+                // 如果众筹目标金额大于实际众筹金额
                 if ($crowdfunding->target_amount > $crowdfunding->total_amount) {
+                    // 调用众筹失败逻辑
                     $this->crowdfundingFailed($crowdfunding);
                 } else {
+                    // 否则调用众筹成功逻辑
                     $this->crowdfundingSucceed($crowdfunding);
                 }
-
             });
     }
 
