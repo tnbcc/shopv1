80d827171276bc031dc4a3ac2b92f7a6fc65ac94
tnbcc <tniub.cc@gmail.com>
2019-02-14 09:31:39 +0000

    开发环境加入 ngrok

--- app/Listeners/UpdateCrowdfundingProductProgress.php
+++ app/Listeners/UpdateCrowdfundingProductProgress.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Listeners;
 
 use App\Events\OrderPaid;
@@ -7,37 +6,36 @@ use App\Models\Order;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class UpdateCrowdfundingProductProgress
+class UpdateCrowdfundingProductProgress implements ShouldQueue
 {
-    /**
-     * Handle the event.
-     *
-     * @param  OrderPaid  $event
-     * @return void
-     */
     public function handle(OrderPaid $event)
     {
         $order = $event->getOrder();
-
+        // 如果订单类型不是众筹商品订单，无需处理
         if ($order->type !== Order::TYPE_CROWDFUNDING) {
-            return ;
+            return;
         }
-
         $crowdfunding = $order->items[0]->product->crowdfunding;
 
         $data = Order::query()
-             ->where('type', Order::TYPE_CROWDFUNDING)
-             ->whereNotNull('paid_at')
-             ->whereHas('items', function ($query) use ($crowdfunding) {
-                 $query->where('product_id', $crowdfunding->product_id);
-             })->first([
-                 \DB::raw('sum(total_amount) as total_amount'),
-                 \DB::raw('count(distinct(user_id)) as user_count'),
+            // 查出订单类型为众筹订单
+            ->where('type', Order::TYPE_CROWDFUNDING)
+            // 并且是已支付的
+            ->whereNotNull('paid_at')
+            ->whereHas('items', function ($query) use ($crowdfunding) {
+                // 并且包含了本商品
+                $query->where('product_id', $crowdfunding->product_id);
+            })
+            ->first([
+                // 取出订单总金额
+                \DB::raw('sum(total_amount) as total_amount'),
+                // 取出去重的支持用户数
+                \DB::raw('count(distinct(user_id)) as user_count'),
             ]);
 
         $crowdfunding->update([
-           'total_amount' => $data->total->total_amount,
-           'user_count'   => $data->user_count,
+            'total_amount' => $data->total_amount,
+            'user_count'   => $data->user_count,
         ]);
     }
 }
--- config/app.php
+++ config/app.php
@@ -232,4 +232,6 @@ return [
     ],
     'order_ttl' => 1800,
 
+    'ngrok_url' => env('NGROK_URL'),
+
 ];
